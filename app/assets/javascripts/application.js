9b26772ac57c68043d680c76b0a55dfd1e2b4daf
Salvador Lopez Mendoza <simiographics@gmail.com>
2014-05-17 20:36:21 -0500

    estilos basicos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,9 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+//     __  ____  ____  ____          __  
+//    / / / / / / / / / / /_  __  __/ /_ 
+//   / /_/ / /_/ / /_/ / __ \/ / / / __ \
+//  / __  / __  / __  / / / / /_/ / /_/ /
+// /_/ /_/_/ /_/_/ /_/_/ /_/\__,_/_.___/ 
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
